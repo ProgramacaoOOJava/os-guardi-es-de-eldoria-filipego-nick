1e505796875ff10fd05c48c48264df56b9c660bf
filipego-nick <152667431+filipego-nick@users.noreply.github.com>
2026-05-03 18:01:03 -0300

    Criaçaõ da classe personagem
    Classe criada conforme as instruções da aula.

--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,19 @@
 public class Personagem {
-    String nome;
-    String classe;
-    int nivel;
-    int pontosDeVida;
-    double poderBase;
+   public String nome,classe;      //Declaração de variáveis
+    public int nivel,pontos_vida;
+    public double poder_base;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
-        this.nome = nome;
-        .
-        .
-        .
-
+    public Personagem(String nome, String Classe, int nivel, int pontos_vida, double poder_base){ //Método construtor do personagem
+        this.nome=nome;                 //Estabelecimento dos atributos do personagem
+        this.classe=Classe;
+        this.nivel=nivel;
+        this.pontos_vida=pontos_vida;
+        this.poder_base=poder_base;
     }
 
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+    public void exibirStatus(){ //Exibe na tela os atributos dos personagens
+        System.out.println("Nome do personagem: "+nome+"\nClasse: "+classe+"\nNivel: "+nivel+"\nPontos de vida: "+pontos_vida+"\nQuantidade de poder: "+poder_base+"\n\n"); /
+        }
     }
-}
+
 
