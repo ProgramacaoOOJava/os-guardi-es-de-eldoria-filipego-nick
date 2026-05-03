e0de8f171820b97a1a44b06f306954794922a95e
filipego-nick <152667431+filipego-nick@users.noreply.github.com>
2026-05-03 17:59:09 -0300

    Classe main criada
    Classe criada conforme as instruções da aula.

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
-
+        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10);
+        Personagem heroi2 = new Personagem("Anão","Humano",3,7,7);
+        p1.exibirStatus();
+        p2.exibirStatus();
         .
         .
         .
